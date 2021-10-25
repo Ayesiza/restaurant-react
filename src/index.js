c2d0aef3128121a4f2f23d36cdb9010fcb367c8d
Ayesiza <yesiha2001@gmail.com>
2021-10-25 14:51:15 +0300

    fetch recipe API and the footer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 
 
 ReactDOM.render(
